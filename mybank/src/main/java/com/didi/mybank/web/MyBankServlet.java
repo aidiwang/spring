3c72a5f66175717dcb527bf7c4e148770c766540
didi <aidiwang77@gmail.com>
2024-03-03 23:06:10 -0500

    fix

--- mybank/src/main/java/com/didi/mybank/web/MyBankServlet.java
+++ mybank/src/main/java/com/didi/mybank/web/MyBankServlet.java
@@ -2,6 +2,8 @@ package com.didi.mybank.web;
 
 import com.didi.mybank.context.Application;
 import com.didi.mybank.model.Transaction;
+import com.didi.mybank.services.TransactionService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,15 +13,17 @@ import java.util.List;
 
 public class MyBankServlet extends HttpServlet {
 
+    private TransactionService transactionService = Application.transactionService;
+    private ObjectMapper objectMapper = Application.objectMapper;
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
         if (request.getRequestURI().equalsIgnoreCase("/transactions")){
             int amount = Integer.parseInt(request.getParameter("amount"));
             String reference = request.getParameter("reference");
 
-            Transaction transaction = Application.transactionService.create(amount, reference);
+            Transaction transaction = transactionService.create(amount, reference);
             response.setContentType("application/json; charset=UTF-8");
-            String json = Application.objectMapper.writeValueAsString(transaction);
+            String json = objectMapper.writeValueAsString(transaction);
             response.getWriter().print(json);
         }else{
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
@@ -39,8 +43,8 @@ public class MyBankServlet extends HttpServlet {
                             "</html>");
         }else if (request.getRequestURI().equalsIgnoreCase("/transactions")){
             response.setContentType("application/json; charset=UTF-8");
-            List<Transaction> transactions = Application.transactionService.findAll();
-            response.getWriter().print(Application.objectMapper.writeValueAsString(transactions));
+            List<Transaction> transactions = transactionService.findAll();
+            response.getWriter().print(objectMapper.writeValueAsString(transactions));
         }
     }
 }
